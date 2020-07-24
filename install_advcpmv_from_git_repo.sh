c42df2cb3b68a9c2c587f07a4d990dedecfada5e
sebma <sebma@users.noreply.github.com>
2020-07-24 11:37:35 +0200

    Updated install_advcpmv_from_git_repo.sh

---
+++ install_advcpmv_from_git_repo.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+scriptName=$(basename "$0")
+scriptDir=$(dirname $0)
+scriptDir=$(cd $scriptDir;pwd)
+download="$(which wget2 2>/dev/null || which wget 2>/dev/null)"
+
+if test "$download"; then
+	download="$download -c"
+else
+	download="$(which curl) -C- -O"
+fi
+
+test "$make" || { which remake >/dev/null && export make="$(which remake) -j$(nproc)" || export make="$(which make) -j$(nproc)"; }
+
+if [ $# != 1 ]; then
+	echo "=> Usage: $scriptName <coreutils_version_number>" 1>&2
+	exit 1
+else
+	version=$1
+fi
+
+typeset -r isAdmin=$(groups 2>/dev/null | egrep -wq "sudo|adm|admin|root" && echo true || echo false)
+if $isAdmin; then
+	prefix=/usr/local
+	sudo="command sudo -H"
+else
+	prefix=$HOME/local
+	sudo=""
+fi
+
+if $download http://ftp.gnu.org/gnu/coreutils/coreutils-$version.tar.xz; then
+	set -x
+	time if tar -xf coreutils-$version.tar.xz; then
+		set +x
+		cd coreutils-$version
+		patch=$(\ls ../advcpmv-* | grep $version)
+		\patch -p1 -i $patch
+#		mv GNUmakefile GNUmakefile.BACKUP
+		test -s Makefile || time ./configure --prefix=$prefix --exec-prefix=$prefix
+		if [ $? = 0 ]; then
+			time if $make; then
+				if test -x src/cp; then
+					cd src
+					\cp -puv cp advcp
+					\cp -puv mv advmv
+					if $sudo install -vpm 755 adv* $prefix/bin/; then
+						if cd ../man; then
+							\cp -puv cp.1 advcp.1
+							\cp -puv mv.1 advmv.1
+							\gzip -9v adv*.1
+							$sudo install -vpm 644 adv* $prefix/share/man/man1/
+						fi
+						cd ../..
+						rm -fr coreutils-$version*
+					fi
+				fi
+			fi
+		fi
+	fi
+fi
